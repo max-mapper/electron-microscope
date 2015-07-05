28fe44ebbbba8b1c74e90584e46929c65c344579
Max Ogden <max@maxogden.com>
2015-07-05 16:13:23 -0700

    initial sort of working version

---
+++ cli.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env electron
+
+var fs = require('fs')
+
+var url = process.argv[2]
+var scriptPath = process.argv[3]
+
+if (!url || !scriptPath) {
+  console.error('Usage: electron-microscope <url> <script>')
+  process.exit(1)
+}
+
+var script = fs.readFileSync(scriptPath)
+  
+var App = require('app')
+var BrowserWindow = require('browser-window')
+var ipc = require('ipc')
+
+var uuid = require('hat')
+
+var microscope = require('./index.js')
+
+App.on('ready', load)
+
+function load () {
+  microscope(BrowserWindow, url, function (err, win, resp, cb) {
+    var id = uuid()
+    
+    ipc.on(id + '-done', function () {
+      cb()
+    })
+    
+    win.webContents.executeJavaScript(wrapRPC(script))
+
+    function wrapRPC (fn) {
+      return ";(function () { " + fn + "; require('ipc').send('" + id + "-done', true); })();"
+    }
+  })
+}
---
+++ index.js
@@ -0,0 +1,59 @@
+var path = require('path')
+
+module.exports = scrape
+
+function scrape (BrowserWindow, pageUrl, cb) {
+  var win
+  win = new BrowserWindow({
+    "web-preferences": {
+      "web-security": true
+    },
+    nodeIntegration: false,
+    show: true
+  })
+  var limit = 10000
+  var loadedPage = false
+  var loadedAll = false
+  var finished = false
+  
+  var limitInterval = setTimeout(function () {
+    if (finished) return
+    finished = true
+    win.close()
+    cb(new Error('Timed out'))
+  }, limit)
+  
+  win.loadUrl(pageUrl)
+  
+  var responseDetails
+  win.webContents.once('did-get-response-details', function (event, status, newUrl, originalUrl, respCode, method, referrer, headers) {
+    responseDetails = {
+      status: status,
+      newUrl: newUrl,
+      originalUrl: originalUrl,
+      httpResponseCode: respCode,
+      requestMethod: method,
+      referrer: referrer,
+      headers: headers
+    }
+  })
+  
+  win.webContents.once('dom-ready', function (ev) {
+    clearInterval(limitInterval)
+    
+    cb(null, win, responseDetails, cleanup)
+    
+    function cleanup () {
+      if (finished) return
+      finished = true
+      win.close()
+    }
+  })
+  
+  win.webContents.once('did-fail-load', function (ev, code, desc) {
+    if (finished) return
+    finished = true
+    win.close()
+    cb(new Error(desc))
+  })
+}
